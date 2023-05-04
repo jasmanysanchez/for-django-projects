2aaf24760888fa4476f9aebfe4b19d0b4320b123
jsunae <106758719+jsunae@users.noreply.github.com>
2023-05-04 12:13:02 -0500

    Initial Commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="for-django-projects",
-    version="1.0.0",
+    version="1.0.1",
     author="Jasmany Sanchez Mendez",
     author_email="jasmanysanchez97@gmail.com",
     description="Package of libraries for Django projects",
